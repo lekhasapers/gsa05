c421a4741d1f19f131984cb38fae6cd7972858b4
Lekha Sapers <lekhasapers@ucsb.edu>
2024-02-21 19:07:02 -0800

    final push

--- shapeFuncs.cpp
+++ shapeFuncs.cpp
@@ -68,8 +68,8 @@ bool pointsApproxEqual(Point p1,
 }
 
 bool boxesApproxEqual(Box b1, Box b2, double tolerance) {
-	bool cornersEqual = approxEqual(b1.ul.x,b2.ul.x, tolerance = 0 ) && approxEqual(b1.ul.y, b2.ul.y, tolerance = 0 );
-	bool dimensionsEqual = approxEqual(b1.width, b2.width, tolerance = 0 ) && approxEqual(b1.height, b2.height, tolerance = 0);
+	bool cornersEqual = pointsApproxEqual(b1.ul,b2.ul, tolerance);
+	bool dimensionsEqual = approxEqual(b1.width, b2.width, tolerance) && approxEqual(b1.height, b2.height, tolerance);
   return cornersEqual && dimensionsEqual;
   }
 
