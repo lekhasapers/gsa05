5839292aa884a286128dff227a6e151f9534beed
Lekha Sapers <lekhasapers@ucsb.edu>
2024-02-21 17:16:12 -0800

    final commit

--- boxToStringTest.cpp
+++ boxToStringTest.cpp
@@ -9,12 +9,15 @@ int main() {
   initPoint(&p2, 3.14159,6.2831853071);
   
   Box b1 = {p1, 5.0, 10.0};
-  Box b2 = {p2, 2.71828, 3.14159};
 
-  assertEquals("ul=(3,4),w=5,h=10", boxToString(b1), "boxToString(b1)");
-  assertEquals("ul=(3.142,6.283),w=2.718,h=3.142", boxToString(b2), "boxToString(b2)");
-  assertEquals("ul=(3,6),w=3,h=3", boxToString(b2,0), "boxToString(b2,0)");
-  assertEquals("ul=(3.1416,6.2832),w=2.7183,h=3.1416", boxToString(b2,4), "boxToString(b2,4)");
+  assertEquals("ul=(3,4),w=5,h=10", boxToString(b1, 1), "boxToString(b1,1)");
+  assertEquals("ul=(3.2,4.2),w=5.2,h=6.2", boxToString(b1, 2), "boxToString(b1, 2)");
+  assertEquals("ul=(3.22,4.22),w=5.22,h=6.22", boxToString(b1, 3), "boxToString(b1, 3)");
+  assertEquals("ul=(3.222,4.222),w=5.222,h=6.222", boxToString(b1,4), "boxToString(b1,4)");
+  assertEquals("ul=(3.2222,4.2222),w=5.2222,h=6.2222", boxToString(b1, 5), "boxToString(b1,5)");
+  assertEquals("ul=(3.22222,4.22222),w=5.22222,h=6.22222", boxToString(b1, 6), "boxToString(b1,6)");
 
   return 0;
 }
+
+
